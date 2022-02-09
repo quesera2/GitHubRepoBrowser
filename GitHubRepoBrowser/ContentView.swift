a534e46f7218fb727a31b4de7fc2e1bd263ce6ae
quesera2 <quesera2262@gmail.com>
2022-02-09 16:11:16 +0900

    Initial Commit

---
+++ GitHubRepoBrowser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubRepoBrowser
+//
+//  Created by quesera2 on 2022/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubRepoBrowser/GitHubRepoBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepoBrowserApp.swift
+//  GitHubRepoBrowser
+//
+//  Created by quesera2 on 2022/02/09.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepoBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
