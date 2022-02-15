24ca821b8e48142663343804753d430154f4ad2c
quesera2 <quesera2262@gmail.com>
2022-02-15 14:57:41 +0900

    DIコンテナをやめて、コンストラクタインジェクションするように修正

---
+++ Model/Sources/Model/GitHubAPIClientProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubAPIClientProtocol.swift
+//  
+//
+//  Created by quesera2 on 2022/02/15.
+//
+
+import Foundation
+
+public protocol GitHubAPIClientProtocol {
+    
+    func fetchRepositories(userName: String) async throws -> [GitHubRepository]
+}
