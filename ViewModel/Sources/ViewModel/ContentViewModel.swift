51207bd35926f796b433fa391170aca7040393b4
quesera2 <quesera2262@gmail.com>
2022-02-17 10:50:59 +0900

    Stateのenumを別ファイルに切り出し

--- ViewModel/Sources/ViewModel/ContentViewModel.swift
+++ ViewModel/Sources/ViewModel/ContentViewModel.swift
@@ -8,19 +8,6 @@
 import Foundation
 import Model
 
-enum ContentViewModelState {
-    /// 待機状態
-    case Idle
-    /// 読込中状態
-    case Loading
-    /// 読み込み成功
-    case Loaded([GitHubRepository])
-    /// 読み込み失敗
-    case Failed(ContentViewModelError)
-}
-
-
-
 @MainActor
 public class ContentViewModel: ObservableObject {
     
