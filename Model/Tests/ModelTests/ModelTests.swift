806ae0abf49c8e7009baa331a3aa0d0817448795
quesera2 <quesera2262@gmail.com>
2022-02-21 11:46:15 +0900

    APIテストのベースを作成

--- Model/Tests/ModelTests/ModelTests.swift
+++ Model/Tests/ModelTests/ModelTests.swift
@@ -1,11 +1,55 @@
 import XCTest
 @testable import Model
 
-final class ModelTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Model().text, "Hello, World!")
+final class GitHubAPIClientTest: XCTestCase {
+    
+    private var client: GitHubAPIClient!
+    
+    override func setUp() {
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockURLProtocol.self]
+        let session = URLSession(configuration: config)
+        self.client = GitHubAPIClient(urlSession: session)
+    }
+    
+    override func tearDown() {
+        MockURLProtocol.requestHandler = nil
+    }
+    
+    func testExample() throws {        
+        XCTAssertEqual("Wow", "Hello, World!")
+    }
+}
+
+// 通信テスト用のモック
+private class MockURLProtocol: URLProtocol {
+    
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else {
+            fatalError()
+        }
+        
+        do {
+            let (response, data)  = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch  {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {
+        // for canceled.
     }
 }
